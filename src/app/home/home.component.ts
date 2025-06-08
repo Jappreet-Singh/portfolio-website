5e3bdc2c2f60d048b2ee58ff99aa446cf348c4b2
Jappreet-Singh <108724577+Jappreet-Singh@users.noreply.github.com>
2025-06-07 23:14:40 -0300

    completed home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,13 @@ import { CommonModule } from '@angular/common';
   template: `
     <section class="home">
       <img src="../assets/laptop_img.jpg" alt="Home Image" class="home-image">
+    <div class = "summary">
+      <p class="summary-text">
+        Hi, I'm <strong>Jappreet Singh</strong>, a passionate software developer with a background in Computer Information Systems from Holland College.
+         I specialize in building web and desktop applications using Angular, Java, Kotlin, and C#. My experience spans front-end and back-end development, including REST APIs, database design, and Agile project execution.
+          With a focus on clean code, problem solving, and user experience. I aim to craft innovative solutions that drive results and improve lives.
+      </p>
+    </div>
     </section>
   `,
   styleUrls: ['./home.component.css']
